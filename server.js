ea968c2be858acd52d122eceeca46a6fee7a91ba
hchung30 <84879834+hchung30@users.noreply.github.com>
2021-11-03 11:58:50 -0400

    update to heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+//components
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+const onHttpStart = () => {
+  console.log("Express http server listening on: " + HTTP_PORT);
+};
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/src/home.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+app.listen(HTTP_PORT, onHttpStart);
